a446da5f8b4c29076bd84ba73e5e2bbab54eb83d
Gabriella Dajka <dajkagabi01@gmail.com>
2024-06-05 21:53:11 +0200

    Első oldal navigáció

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:base_2024/pages/second_page.dart';
 import 'package:flutter/material.dart';
 
 class FirstPage extends StatelessWidget {
@@ -10,17 +9,14 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Első oldal")),
+      //  drawer: Drawer(),
       backgroundColor: Colors.green[200],
       body: Center(
         child: ElevatedButton(
             child: Text("Tovább a második oldalra."),
             onPressed: () {
               //Navigáció
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SecondPage(),
-                  ));
+              Navigator.pushNamed(context, '/secondpage');
             }),
       ),
     );
