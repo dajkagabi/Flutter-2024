fc1757d6a77c5b6be84b602b40cee74e0dfcec6b
Gabriella Dajka <dajkagabi01@gmail.com>
2024-06-06 12:42:07 +0200

    Beállítás

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -5,6 +7,10 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text("Beállítás"),
+      ),
+    );
   }
 }
